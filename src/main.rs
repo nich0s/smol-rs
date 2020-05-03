9322d5a155c2aba954163381e8dc43b92ed2a7e3
nich0s <nich0s@users.noreply.github.com>
2020-05-03 15:17:50 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,19 @@ use sqlite::open;
 use std::fs::File;
 use std::io::prelude::*;
 use rand::seq::SliceRandom;
+use std::time::Instant;
+use std::path::Path;
+use std::io::{BufReader, Error};
 
 async fn post_url(long_url: web::Path<String>) -> impl Responder {
     let connection = sqlite::open(":memory:").unwrap();
     let shortened_url = generate_url();
     let mut statement = connection
-        .prepare("INSERT INTO url (long, short) VALUES (?, ?);").unwrap();
+        .prepare("INSERT INTO url (long, short) VALUES (?, ?);")
+        .unwrap();
 
     statement.bind(1, long_url.as_bytes()).unwrap();
-    statement.bind(2, shortened_url).unwrap();
+    statement.bind(2, shortened_url.as_bytes()).unwrap();
 
     HttpResponse::Ok().body("ok.")
 }
@@ -21,17 +25,43 @@ async fn get_url() -> impl Responder {
     HttpResponse::Ok().body("")
 }
 
-fn generate_url() {
-    let filename = "words.txt";
+fn generate_url() -> String {
+    let path = Path::new("src/words.txt");
+    let display = path.display();
+    println!("{:?}", display);
+    match File::open(&path) {
+        Err(error) => {
+            panic!("{:?}", error)
+        }
+        Ok(file) => {
+            let mut copy = file.try_clone().unwrap();
+            let reader = BufReader::new(copy);
+            let mut content: Vec<String> = vec![];
+            for line in reader.lines() {
+                content.push(line.unwrap());
+            }
+
+            let mut string_builder = String::new();
+            for i in 0..3 {
+                string_builder.push_str(content.choose(&mut rand::thread_rng()).unwrap());
+            }
+
+            println!("{}", string_builder);
+            return string_builder;
+        }
+    };
 }
 
 fn create_database_schema() {
-    // db is in memory - haHAA - no drops
     println!("Initializing database...");
+    let start = Instant::now();
     let connection = sqlite::open(":memory:").unwrap();
     connection
-        .execute("CREATE TABLE url(id INT PRIMARY KEY AUTOINCREMENT NOT NULL, long TEXT NOT NULL, short TEXT NOT NULL);")
+        .execute("CREATE TABLE url(id INTEGER PRIMARY KEY AUTOINCREMENT, long TEXT NOT NULL, short TEXT NOT NULL);")
         .unwrap();
+    let end = Instant::now();
+    let duration = end.duration_since(start);
+    println!("Database initialized in {:?}!", duration);
 }
 
 #[actix_rt::main]
